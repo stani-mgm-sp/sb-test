589a292f7fcb39e3229143599ac826427b35d946
Stanislav Bilik <stanislav.bilik@mgm-sp.com>
2016-07-05 10:33:03 +0200

    code clean up.

--- src/main/java/sb/test/MyDemo.java
+++ src/main/java/sb/test/MyDemo.java
@@ -1,19 +1,17 @@
 package sb.test;
 
-import java.io.IOException;
-
 public class MyDemo {
 
 	public static void main(String[] args) {
 		String s = "";
 		if (s != null) {
-			
+
 		}
 		try {
-			
+
 		} catch (Exception e) {
 		}
-		if (s == null) {
+		if (s == null || true) {
 			System.out.println("X");
 		}
 	}
