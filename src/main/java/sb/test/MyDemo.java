d8d8deccfd3ef96e49bbd5ff3ef5ca589e21039c
Stanislav Bilik <stanislav.bilik@mgm-sp.com>
2016-07-05 11:27:25 +0200

    This reverts commit 00fb5017070eb301afff9a2a6d44d6c85f8a725a. Add some functional features.

--- src/main/java/sb/test/MyDemo.java
+++ src/main/java/sb/test/MyDemo.java
@@ -3,16 +3,6 @@ package sb.test;
 public class MyDemo {
 
 	public static void main(String[] args) {
-		String s = "";
-		if (s != null) {
-
-		}
-		try {
-
-		} catch (Exception e) {
-		}
-		if (s == null || true) {
-			System.out.println("X");
-		}
+		System.out.println("sysout only");
 	}
 }
