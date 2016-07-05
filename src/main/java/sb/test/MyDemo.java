0f8a47d5026918f0ba718ca8f9e03329cf9d6a2c
stani-mgm-sp <stanislav.bilik@mgm-sp.com>
2016-07-05 13:50:14 +0200

    new comment (remote)

--- src/main/java/sb/test/MyDemo.java
+++ src/main/java/sb/test/MyDemo.java
@@ -3,6 +3,7 @@ package sb.test;
 public class MyDemo {
 
 	public static void main(String[] args) {
-		System.out.println("sysout only");
+		System.out.println("sysout only");´
+		// new comment (remote)
 	}
 }
